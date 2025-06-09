16b5bfe405882599666677cde05215e53c8cac99
DarkDzi <eduardoassisaraujo@gmail.com>
2025-06-09 08:28:10 -0300

    Geração do Generate direto no Banco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,7 @@
 CREATE TABLE IF NOT EXISTS qrcodes (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome_arquivo VARCHAR(255) NOT NULL,
-    url VARCHAR(255) NOT NULL
+    image BLOB,
+    QRUrl VARCHAR(255) NOT NULL
     );
 
-INSERT INTO qrcodes (nome_arquivo, url) VALUES
- ('QrCode_101.png', 'https://exemplo.com/101'),
-('QrCode_102.png', 'https://exemplo.com/102');
