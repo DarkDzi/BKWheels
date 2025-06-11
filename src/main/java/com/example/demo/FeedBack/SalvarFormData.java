6a2e5ce54c6afc5817d31377609b78245f20315c
DarkDzi <eduardoassisaraujo@gmail.com>
2025-06-11 09:24:49 -0300

    Mudança css

--- src/main/java/com/example/demo/FeedBack/SalvarFormData.java
+++ src/main/java/com/example/demo/FeedBack/SalvarFormData.java
@@ -4,9 +4,7 @@ import com.example.demo.Modelos.QRCodeData;
 
 import javax.imageio.ImageIO;
 import java.io.ByteArrayOutputStream;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Date;
 
 
@@ -14,13 +12,36 @@ public class SalvarFormData {
     private String Url = "jdbc:h2:file:./data";
     private String Usuario = "admin";
     private String Senha = "123";
+    public boolean tabelaExiste(String nomeTabela) {
+        try (Connection conn = DriverManager.getConnection(Url, Usuario, Senha)) {
+            DatabaseMetaData meta = conn.getMetaData();
 
+            try (ResultSet rs = meta.getTables(null, null, nomeTabela.toUpperCase(), null)) {
+                return rs.next();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
     public void Save(int nota, String comentario, Boolean reparo) {
 
         Date data = new Date();
         java.sql.Timestamp datasql = new java.sql.Timestamp(data.getTime());
         try(Connection conn = DriverManager.getConnection(Url, Usuario, Senha)){
 
+            String sqltable = "CREATE TABLE formdata (" +
+                    "    id IDENTITY PRIMARY KEY," +
+                    "    nota INT NOT NULL CHECK (nota BETWEEN 1 AND 5)," +
+                    "    comentario VARCHAR(1000)," +
+                    "    reparo BOOLEAN,";
+
+            Statement ststable = conn.createStatement();
+            boolean existe = tabelaExiste("formdata");
+            if(!existe) {
+                ststable.execute(sqltable);
+            }
             String sql = "INSERT INTO formdata (nota, comentario, reparo, data) VALUES(?, ?, ?, ?)";
             PreparedStatement sts = conn.prepareStatement(sql);
 
